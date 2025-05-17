9eb2c80eb7a9525b3a035f6eb4555b2dd7f14094
irem-dincer <irem.dincer02@gmail.com>
2025-05-17 16:30:34 +0300

    all done

---
+++ src/main/java/org/example/models/Hamburger.java
@@ -0,0 +1,121 @@
+package org.example.models;
+
+public class Hamburger {
+
+private  String name;
+private  String meat;
+private double price;
+private String breadRollType;
+
+    private String addition1Name;
+    private   double addition1Price;
+    private String addition2Name;
+    private  double addition2Price;
+    private  String addition3Name;
+    private   double addition3Price;
+    private String addition4Name;
+    private   double addition4Price;
+
+    public Hamburger(String name, String meat, double price, String breadRollType){
+        this.name=name;
+        this.meat=meat;
+        this.price=price;
+        this.breadRollType=breadRollType;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setMeat(String meat) {
+        this.meat = meat;
+    }
+
+    public String getMeat() {
+        return this.meat;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        double totalPrice = this.price;
+
+        if(addition1Name != null) {
+            totalPrice += addition1Price;
+        }
+        if(addition2Name != null) {
+            totalPrice += addition2Price;
+        }
+        if(addition3Name != null) {
+            totalPrice += addition3Price;
+        }
+        if(addition4Name != null) {
+            totalPrice += addition4Price;
+        }
+
+        return totalPrice;  // Toplam fiyat (hamburger + eklentiler)
+    }
+
+    public void setBreadRollType(String breadRollType) {
+        this.breadRollType = breadRollType;
+    }
+
+    public String getBreadRollType() {
+        return this.breadRollType;
+    }
+
+
+    public void  addHamburgerAddition1(String addition1Name, double addition1Price){
+this.addition1Name=addition1Name;
+this.addition1Price=addition1Price;
+        System.out.println("extra: "+addition1Name+" price:"+addition1Price);
+    }
+    public void  addHamburgerAddition2(String addition2Name,double addition2Price){
+        this.addition2Name=addition2Name;
+        this.addition2Price=addition2Price;
+        System.out.println("extra: "+addition2Name+" price:"+addition2Price);
+    }
+    public void  addHamburgerAddition3(String addition3Name,double addition3Price){
+        this.addition3Name=addition3Name;
+        this.addition3Price=addition3Price;
+        System.out.println("extra: "+addition3Name+" price:"+addition3Price);
+    }
+    public void  addHamburgerAddition4(String addition4Name,double addition4Price){
+        this.addition4Name=addition4Name;
+        this.addition4Price=addition4Price;
+        System.out.println("extra: "+addition4Name+" price:"+addition4Price);
+    }
+    public String itemizeHamburger() {
+
+        System.out.println("extra:" + addition1Name);
+        System.out.println("extra:" + addition2Name);
+        System.out.println("extra:" + addition3Name);
+        System.out.println("extra:" + addition4Name);
+
+
+        double totalPrice = this.price;
+
+
+        if(addition1Name != null) {
+            totalPrice += addition1Price;
+        }
+        if(addition2Name != null) {
+            totalPrice += addition2Price;
+        }
+        if(addition3Name != null) {
+            totalPrice += addition3Price;
+        }
+        if(addition4Name != null) {
+            totalPrice += addition4Price;
+        }
+
+        return "Hamburger: " + this.getName() + " Hamburger + Tüm malzemeler: " + totalPrice;
+    }
+
+}
+
+
---
+++ src/main/java/org/example/models/HealthyBurger.java
@@ -0,0 +1,60 @@
+package org.example.models;
+
+public class HealthyBurger extends Hamburger{
+private String  healthyExtra1Name;
+private double  healthyExtra1Price;
+private String  healthyExtra2Name;
+private double  healthyExtra2Price;
+
+
+public HealthyBurger(String name,double price,String breadType){
+    super(name,"Tofu",price,breadType);
+
+}
+    public HealthyBurger(String name, double price) {
+        super(name, "tofu", price, "Brown rye");
+    }
+
+public void addHealthyAddition1(String healthyExtra1Name, double healthyExtra1Price){
+    this.healthyExtra1Name=healthyExtra1Name;
+    this.healthyExtra1Price=healthyExtra1Price;
+    System.out.println("Extra: "+healthyExtra1Name+" price: "+healthyExtra1Price);
+}
+    public void addHealthyAddition2(String healthyExtra2Name, double healthyExtra2Price){
+        this.healthyExtra2Name=healthyExtra2Name;
+        this.healthyExtra2Price=healthyExtra2Price;
+        System.out.println("Extra: "+healthyExtra2Name+" price: "+healthyExtra2Price);
+    }
+
+@Override
+public String itemizeHamburger(){
+    System.out.println("extra:"+healthyExtra1Name);
+    System.out.println("extra:"+healthyExtra2Name);
+
+    double totalPrice=super.getPrice();
+
+    if(healthyExtra1Name != null){
+        totalPrice +=healthyExtra1Price;
+    }
+    if(healthyExtra2Name != null){
+        totalPrice +=healthyExtra2Price;
+    }
+    return  "HealthyHamburger : "+getName()+" HealthyHamburger  + Tüm malzemeler: "+totalPrice;
+
+}
+    @Override
+    public double getPrice() {
+        double totalPrice = super.getPrice();  // Hamburger fiyatı + hamburger eklentileri
+
+        if(healthyExtra1Name != null) {
+            totalPrice += healthyExtra1Price;
+        }
+        if(healthyExtra2Name != null) {
+            totalPrice += healthyExtra2Price;
+        }
+
+        return totalPrice;  // Toplam fiyat (hamburger + hamburger eklentileri + healthyBurger eklentileri)
+    }
+
+
+}
